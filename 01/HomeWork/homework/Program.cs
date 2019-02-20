198c6ec64d8a451dee9e84858b7e6c684055b086
Alex <wildkloun@yandex.ru>
2019-02-20 21:47:27 +0300

    Lesson 01 class work add

---
+++ 01/HomeWork/homework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace demoapp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите имя");
+			string name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Здравствуйте, {name}!");
+			Thread.Sleep(5000);
+			Console.WriteLine("Пока, {0}!", name);
+			Console.ReadKey();
+		}
+	}
+}
