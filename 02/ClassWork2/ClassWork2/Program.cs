e0cfc61216a9a39fa5219da2ab15e032d1db4044
Alex <wildkloun@yandex.ru>
2019-02-27 21:33:56 +0300

    classwork3 added

---
+++ 02/ClassWork2/ClassWork2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassWork2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введи радиус круга");
+			double r = double.Parse(Console.ReadLine());
+			double s = Math.PI * Math.Pow(r,2);
+			Console.WriteLine("Площадь круга: " + s);
+			Console.ReadKey();
+		}
+	}
+}
---
+++ 03/ClassWork3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var num1 = 3.14f;
+			//var num2 = 1d;
+			//var num3 = 49l;
+			//var num4 = (byte)255;
+			//Console.WriteLine(num1.GetType());
+			//Console.WriteLine(num2.GetType());
+			//Console.WriteLine(num3.GetType());
+			//Console.WriteLine(num4.GetType());
+			//Console.ReadKey();
+			//Console.WriteLine($"{default(int)}");
+			string [] array = new string[5];
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine($"Введите число {i + 1}");
+				array[i] = Console.ReadLine();
+			}
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(array[i]);
+			}
+			Console.ReadKey();
+		}
+	}
+}
