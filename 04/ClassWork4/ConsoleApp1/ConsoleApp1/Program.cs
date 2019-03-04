46c14780c4d4222f7ae927196c4c0fa7e573f8df
Alex <wildkloun@yandex.ru>
2019-03-04 21:38:47 +0300

    classwork4 added

---
+++ 04/ClassWork4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Введите a: ");
+			//double a = double.Parse(Console.ReadLine());
+			//Console.WriteLine("Введите h: ");
+			//double h = double.Parse(Console.ReadLine());
+			//Console.WriteLine("S боковой поверхности = " + 3 * a * h);
+			//Console.WriteLine("S пирамиды = " + 3 / 2 * a * (a * Math.Sqrt(3) + 2 * h));
+			//Console.WriteLine("V пирамиды = " + Math.Pow(a, 2) / 2 * Math.Sqrt(3) * h / Math.Sqrt(2));
+			string b = "4334";
+			int a = int.Parse(b);
+			Console.WriteLine(a);
+			Console.ReadKey();
+		}
+	}
+}
+  
