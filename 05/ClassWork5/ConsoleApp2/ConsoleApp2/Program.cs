fe3cf6d09b4131007a9611d06813cbf3936d6101
Alex <wildkloun@yandex.ru>
2019-03-06 21:33:54 +0300

    classwork5

---
+++ 05/ClassWork5/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		enum Colors
+		{
+			Black=0x0,
+			Blue=0x1,
+			Cyan= 0x1 << 1,
+			Grey= 0x1 << 2,
+			Green= 0x1 << 3,
+			Magenta= 0x1 << 4,
+			Red= 0x1 << 5,
+			White= 0x1 << 6,
+			Yellow= 0x1 << 7
+		}
+		static void WriteByteValueWithBits(byte value)
+		{
+			Console.WriteLine(
+				"0x{0}\t({1})\t{2}",
+				value.ToString("x").PadLeft(2, '0'),
+				Convert.ToString(value, 2).PadLeft(8, '0'),
+				value.ToString().PadLeft(3, '0'));
+		}
+		static void Main(string[] args)
+		{
+			Colors notColor = 0;
+			object color;
+			for (int i = 0; i < 4; i++)
+			{
+				Console.WriteLine($"Введите {i} из четырех: ");
+				if (Enum.TryParse(typeof(Colors), Console.ReadLine(), true, out color))
+					notColor = notColor | (Colors)color;
+			}
+			WriteByteValueWithBits((byte)notColor);
+			//int?[] array = new int?[4];
+			for(int i = 0; i < 9; i++)
+			{
+				if ((notColor & (Colors)i) == (Colors)i)
+
+			}
+			Console.ReadKey();
+		}
+	}
+}
+
+//ввести 4 цвета
+//вывести введенные цвета
+//вывести невошедшие цвета
---
+++ 05/ClassWork5/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число: ");
+			try
+			{
+				int a = int.Parse(Console.ReadLine());
+				if (a > 3)
+					throw new Exception("ОШИБКА");
+				Console.WriteLine("Число " + ((a < 0) ? "отрицательное" : "неотрицательное"));
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine($"{e.GetType()},{e.Message}");
+				
+			}
+			finally
+			{
+				Console.WriteLine("fdfdf");
+			}
+			Console.ReadKey();
+		}
+	}
+}
