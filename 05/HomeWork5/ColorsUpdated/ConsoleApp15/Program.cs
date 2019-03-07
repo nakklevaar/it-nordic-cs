07dac29691e70e0281ffc5b60ba1bcbb8a32aa22
unknown <wildkloun@yandex.ru>
2019-03-07 08:40:29 +0300

    homework5 colors updated

---
+++ 05/HomeWork5/ColorsUpdated/ConsoleApp15/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        enum Colors
+        {
+            Black,
+            Blue,
+            Cyan,
+            Grey,
+            Green,
+            Magenta,
+            Red,
+            White,
+            Yellow
+        }
+        static void Main(string[] args)
+        {
+            Colors[] chosen = new Colors[4];
+            Colors?[] notChosen = new Colors?[15];
+            Console.WriteLine("Список возможных цветов: " + string.Join(" - ", Enum.GetNames(typeof(Colors))) + "\n");
+            object color;
+            for (int i = 0; i < 4; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine($"Введите {i + 1} цвет из четырех: ");
+                    string value = Console.ReadLine();
+                    if (!int.TryParse(value, out int ignoreMe) && Enum.TryParse(typeof(Colors), value, true, out color))
+                    {
+                        chosen[i] = (Colors)color;
+                        break;
+                    }
+                    Console.WriteLine("Ошибка, введите цвет правильно.");
+                }
+            }
+            int m = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if ((Colors)i == chosen[j])
+                        break;
+                    else if (j == 3)
+                    {
+                        notChosen[m] = (Colors)i;
+                        m++;
+                    }
+                }
+            }
+            Print(chosen, notChosen);
+            Console.ReadKey();
+        }
+        static void Print(Colors[] chosen, Colors?[] notChosen)
+        {
+            Console.WriteLine();
+            string s = "";
+            foreach (var k in chosen)
+            {
+                if (!s.Contains(k.ToString()))
+                    s = s + k + " ";
+            }
+            Console.WriteLine("\n \n" + "Избранные цвета: " + s + "\n");
+            Console.Write("Невошедшие: ");
+            foreach (var k in notChosen)
+            {
+                if (k != null)
+                    Console.Write(k + " ");
+            }
+        }
+    }
+}
+
+//ввести 4 цвета
+//вывести введенные цвета
+//вывести невошедшие цвета
