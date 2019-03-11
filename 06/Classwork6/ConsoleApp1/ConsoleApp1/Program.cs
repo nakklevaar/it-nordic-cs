4237ecdc8d8467193d3006f8cac5ab342064414e
Alex <wildkloun@yandex.ru>
2019-03-11 21:20:51 +0300

    classwork6 added

---
+++ 06/Classwork6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var marks = new[]
+			{
+				new [] {2,3,3,2,3},
+				new [] {2,4,5,3},
+				null,
+				new [] {5,5,5,5},
+				new [] {4}
+			};
+
+			int p = marks.Length;
+			while (p < 5)
+			{
+				int sum = 0;
+				for (int i = 0; i < marks[p].Length; i++)
+				{
+					
+				}
+			}
+		}
+	}
+}
