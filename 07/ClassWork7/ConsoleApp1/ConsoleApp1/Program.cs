3b85d8ae205cdcf60224be89110de452ad49fedc
Alex <wildkloun@yandex.ru>
2019-03-13 21:16:19 +0300

    ClassWork7 added

---
+++ 07/ClassWork7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.Write("Введите 1 число: ");
+			//double a = double.Parse(Console.ReadLine());
+			//Console.Write("Введите 2 число: ");
+			//double b = double.Parse(Console.ReadLine());
+			//Console.WriteLine(a + " * " + b + " = " + a * b);
+			//Console.WriteLine(string.Format($"{a:#.##} + {b:#.##} = {a+b:#.##}"));
+			//Console.WriteLine($"{a} - {b} = {a-b}");
+			string test = "my test string";
+			Console.WriteLine(test.Substring(8));
+			Console.ReadKey();
+		}
+	}
+}
