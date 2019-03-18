58963f75ed7ab9232ce639f10fbc532ad7f8b8ee
Alex <wildkloun@yandex.ru>
2019-03-18 21:26:32 +0300

    ClassWork8 added

---
+++ 08/ClassWork8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//var countries = new Dictionary <string, string>
+			//{
+			//	{"russia","moscow" },
+			//	{"usa","washington" },
+			//	{"britan","london" }
+			//};
+			//Random rnd = new Random();
+			//while (true)
+			//{
+			//	int index = rnd.Next(4);
+			//	var pair = countries.ElementAt(index);
+			//	Console.WriteLine("введите столицу " + pair.Key);
+			//	if (countries[pair.Key] == Console.ReadLine())
+			//		Console.WriteLine("good, go next");
+			//	else
+			//		break;
+			//}
+			//Console.WriteLine("lost");
+			/////////////////////////////////////////////
+			//Queue<int> que = new Queue<int>();
+			//string read;
+			//int s;
+			//while (true)
+			//{
+			//	Console.WriteLine("Введите число,'run','exit'");
+			//	 read = Console.ReadLine();
+			//	if (read == "run" || read == "exit")
+			//	{
+			//		if (int.TryParse(read, out s))
+			//		{
+			//			break;
+			//		}
+			//	}
+			//	Console.WriteLine("Ошибка");
+			//}
+			//if (read == "run")
+			//{
+			//	que.Enqueue(s);
+			//}
+			////////////////////////////////////////////
+			
+
+			Console.ReadKey();
+		}
+	}
+}
