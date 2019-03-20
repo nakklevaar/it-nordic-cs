f0758854345936e06e9c233dd7cb666253ea78d0
Alex <wildkloun@yandex.ru>
2019-03-20 21:29:20 +0300

    classwork9 added

---
+++ 09/ClassWork9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				int[] initialArray = GetTestArray(2*i*1000, 1_000_000);
+				Stopwatch stopwatch = new Stopwatch();
+				//WriteArrayState("Initial state:", initialArray);
+				int[] bubbleSortedArray = (int[])initialArray.Clone();
+				stopwatch.Start();
+				BubleSort(bubbleSortedArray);
+				stopwatch.Stop();
+				Console.WriteLine($"Bubble sort done in {stopwatch.ElapsedMilliseconds} ms): ");
+				int[] dotNetSort = (int[])initialArray.Clone();
+				stopwatch.Restart();
+				Array.Sort(dotNetSort);
+				Console.WriteLine($".Net sort done in {stopwatch.ElapsedMilliseconds} ms \n");
+				//WriteArrayState("Bubbled state:", bubbleSortedArray);
+			}
+		}
+		static int[] GetTestArray(int length,int maxValue)
+		{
+			var arr = new int[length];
+
+			var rnd = new Random();
+			for (var i = 0; i < arr.Length; i++)
+			{
+				arr[i] = rnd.Next(maxValue);
+			}
+
+			return arr;
+		}
+		static void WriteArrayState(string message, int[] arr)
+		{
+			Console.WriteLine(message);
+			for (var i = 0; i < arr.Length; i++)
+			{
+				Console.WriteLine(arr[i]);
+			}
+		}
+		static void BubleSort(int[] arr)
+		{
+			for (int i = 0; i < arr.Length-1; i++)
+			{
+				for (int j = 0; j < arr.Length-1-i; j++)
+				{
+					if (arr[j] > arr[j + 1])
+					{
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					}
+				}
+
+			}
+		}
+	}
+}
---
+++ 09/ClassWork9/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 3, 1, 4, 2 };
+			string x = "test";
+			Met(arr, x);
+			Console.WriteLine(string.Join(' ', arr));
+			Console.WriteLine(x);
+		}
+		static void Met(int[] arr, string x)
+		{
+			arr[3] = 10;
+			x = x+ "trggfgf";
+		}
+	}
+}
