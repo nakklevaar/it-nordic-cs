1a11802ab9dd96de290a1403405070d79ce13f5b
unknown <wildkloun@yandex.ru>
2019-03-26 01:14:40 +0300

    homework10 classes added

---
+++ 10/HomeWork10/ConsoleApp27/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp27
+{
+    class Person
+    {
+        public string Name;
+        int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (_age > 0 && _age < 140)
+                {
+                    _age = value;
+                }
+                else throw new IndexOutOfRangeException();
+            }
+        }
+        public void PrintPlus4()
+        {
+            Console.WriteLine($"Name: {Name}, age  in 4 years: {Age + 4}");
+        }
+    }
+}
