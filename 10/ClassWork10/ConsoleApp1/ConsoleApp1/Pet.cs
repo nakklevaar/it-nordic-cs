038317cfc2f245a0d4c5e1ff36134f5c4c05c84c
Alex <wildkloun@yandex.ru>
2019-03-25 21:20:07 +0300

    lesson 10 classwork added

---
+++ 10/ClassWork10/ConsoleApp1/ConsoleApp1/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		char _sex;
+		public char Sex
+		{
+			get
+			{
+				return _sex;
+			}
+			set
+			{
+				if (value == 'F' || value == 'M')
+					_sex = value;
+				else _sex = '?';
+			}
+		}
+		public int Age;
+		public void Write()
+		{
+			Console.WriteLine($"Name: {Name}, Age: {Age}, Kind: {Kind}, Sex: {Sex}");
+		}
+	}
+}
---
+++ 10/ClassWork10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet cat = new Pet()
+			{
+				Name = "Кошка",
+				Age = 3,
+				Kind = "Usual",
+				Sex = 'M'
+			};
+			cat.Write();
+			Pet dog = new Pet()
+			{
+				Name = "Собака",
+				Age = 7,
+				Kind = "Buldog",
+				Sex = 'F'
+			};
+			dog.Write();
+			Pet fish = new Pet()
+			{
+				Name = "Рыба",
+				Age = 1,
+				Kind = "Trout",
+				Sex = ' '
+			};
+			fish.Write();
+			Console.ReadKey();
+		}
+	}
+}
