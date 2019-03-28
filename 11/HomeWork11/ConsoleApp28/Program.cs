b2ba2d8d0db47542ae2cf65a9fc057880f535df0
unknown <wildkloun@yandex.ru>
2019-03-28 20:55:49 +0300

    Homework11 alarm added

---
+++ 11/HomeWork11/ConsoleApp28/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReminderItem p1 = new ReminderItem(DateTimeOffset.Parse("29-03-2019"));
+            p1.WriteProperties();
+
+            Console.WriteLine();
+
+            ReminderItem p2 = new ReminderItem(DateTimeOffset.Parse("18-06-2019 23:34"));
+            p2.WriteProperties();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11/HomeWork11/ConsoleApp28/ReminderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    class ReminderItem
+    {
+        DateTimeOffset AlarmDate { get; set; }
+        string AlarmMessage
+        {
+            get
+            {
+                return $"Будильник установлен на {AlarmDate.ToString("MM/dd/yyyy HH:mm:ss")}";
+            }
+        }
+
+        TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return AlarmDate.Subtract(DateTimeOffset.Now);
+            }
+        }
+
+        bool IsOutdated
+        {
+            get
+            {
+                if (AlarmDate <= DateTimeOffset.Now)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine($"{nameof(AlarmDate)} : {AlarmDate.ToString("MM/dd/yyyy HH:mm:ss")}");
+            Console.WriteLine($"{nameof(AlarmMessage)} : {AlarmMessage}");
+            Console.WriteLine($"{nameof(TimeToAlarm)} : {TimeToAlarm.ToString("c")}");
+            Console.WriteLine($"{nameof(IsOutdated)} : {IsOutdated}");
+        }
+
+        public ReminderItem(DateTimeOffset AlarmDate)
+        {
+            this.AlarmDate = AlarmDate;
+            Console.WriteLine(AlarmMessage);
+        }
+    }
+}
