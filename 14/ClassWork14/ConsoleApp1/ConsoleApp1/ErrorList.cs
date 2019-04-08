1df6a58a69df6300e57143a4f3b891a992d5e89e
Alex <wildkloun@yandex.ru>
2019-04-08 21:30:50 +0300

    classwork14 added

---
+++ 14/ClassWork14/ConsoleApp1/ConsoleApp1/ErrorList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class ErrorList : IDisposable, IEnumerable<string>
+	{
+		public static string OutputPrefixFormat { get; set; }
+
+		static ErrorList()
+		{
+			OutputPrefixFormat = "MMMM d, yyyy (hh:mm tt)";
+		}
+
+		public string Category { get; }
+		private List<string> _errors;
+
+		public ErrorList(string category)
+		{
+			Category = category;
+			_errors = new List<string>();
+		}
+
+		public void Dispose()
+		{
+			if(_errors != null)
+			{
+				_errors.Clear();
+				_errors = null;
+			}
+		}
+
+		public void WriteToConsole()
+		{
+			foreach (var error in _errors)
+			{
+				Console.WriteLine(DateTime.Now.ToString(OutputPrefixFormat) + " "+error);
+			}
+		}
+
+		public void Add(string errorMessage)
+		{
+			_errors.Add(errorMessage);
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return _errors.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ 14/ClassWork14/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var err = new ErrorList("type");
+			err.Add("type error1");
+			err.Add("type error12");
+			err.Add("type error1235");
+
+			err.WriteToConsole();
+		}
+	}
+}
