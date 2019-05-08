e47b22df2be2974128abebf835e7ff76b23b9c18
MakarevichAV <redishtale@gmail.com>
2019-05-08 21:45:29 +0300

    classwork22 added

---
+++ 22/ClassWork22/WebApplication1/WebApplication1/Controllers/CitiesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DataStore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	[Route("/api/cities")]
+	public class CitiesController: Controller
+	{
+		[HttpGet()]
+		public IActionResult GetCities()
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var cities = citiesDataStore.Cities;
+				return Ok(cities);
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetCity(int id)
+		{
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			var city = citiesDataStore.Cities
+				.Where(x => x.Id == id)
+				.FirstOrDefault();
+
+			if (city == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(city);
+		}
+
+		[HttpPost()]
+		public IActionResult CreateCity([FromBody] CityCreateModel city)
+		{
+			if(city==null)
+			{
+				BadRequest();
+			}
+
+			var citiesDataStore = CitiesDataStore.GetInstance();
+			int newCityId = citiesDataStore.Cities.Max(c => c.Id) + 1;
+
+			var newCity = new CityGetModel
+			{
+				Id = newCityId,
+				Name = city.Name,
+				Description = city.Description,
+				NumberOfPointsOfInterest = city.NumberOfPointsOfInterest
+			};
+
+			citiesDataStore.Cities.Add(newCity);
+
+			return CreatedAtRoute("GetCity", new { id = newCityId }, newCity);
+		}
+	}
+}
